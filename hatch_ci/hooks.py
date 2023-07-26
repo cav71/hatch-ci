a39b92edadc457c8b439bcfbd0b38679576af33b
antonio <a.cavallo@cavallinux.eu>
2023-07-26 10:19:57 +0200

    initial

--- hatch_ci/hooks.py
+++ hatch_ci/hooks.py
@@ -1,20 +1,8 @@
 from hatchling.plugin import hookimpl
 
-from hatch_ci.build_hook import GitHubBuildHook
-from hatch_ci.metadata_hook import GitHubMetadataHook
-from hatch_ci.version_hook import GitHubVersionSource
+from hatch_ci.version_hook import CIVersionSource
 
 
 @hookimpl
 def hatch_register_version_source():
-    return GitHubVersionSource
-
-
-@hookimpl
-def hatch_register_build_hook():
-    return GitHubBuildHook
-
-
-@hookimpl
-def hatch_register_metadata_hook():
-    return GitHubMetadataHook
+    return CIVersionSource
